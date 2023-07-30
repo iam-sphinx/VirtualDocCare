119aefb7a985d9f39786f03f6245d9ec5181f594
iam-sphinx <kandarp1803@gmail.com>
2023-07-30 22:54:38 +0530

    feat: :sparkles: Modified Specialist Section

--- frontend/components/Specialities.jsx
+++ frontend/components/Specialities.jsx
@@ -1,107 +1,34 @@
-import React from 'react';
+import React from "react";
 import Image from "next/image";
-
+import SpecialistCard from "./SpecialistCard";
+const CardImage = [
+  { img_src: "/specialists/brain.png", title: "Neurologist" },
+  { img_src: "/specialists/cardio.png", title: "Cardiologist" },
+  { img_src: "/specialists/dentist.png", title: "Dentist" },
+  { img_src: "/specialists/dermatologist.png", title: "Dermatologist" },
+  { img_src: "/specialists/eye.png", title: "Eye-Specialist" },
+  { img_src: "/specialists/gynecology.png", title: "Gynocologist" },
+  { img_src: "/specialists/pCare.png", title: "Primary Doctor" },
+  { img_src: "/specialists/psychiatrist.png", title: "Psychiatrist" },
+];
 
 const Specialities = () => {
   return (
-    <div className='w-full'>
-        <p className=' ml-20 text-5xl mt-16 font-extrabold text-[#28282B]'>Top-searched specialities </p>
+    <div className="w-full px-32">
+      <h1 className="text-[#363c45] font-semibold text-2xl mt-32 mb-16">
+        Most Searched Specialists: Your Trusted Guides to Expert Care!
+      </h1>
 
-   <div className="mt-10  lg:grid grid-cols-4  gap-8 mx-32">
-
-    {/* //card 1 primarycare */}
-
-   <div className="bg-white h-[20rem] w-[20rem] shadow-2xl  border rounded-xl" >
-
-   <div className="text-black flex flex-col items-center mt-2">
- <img  className="  flex h-[16rem]" src="/primarycare.jpg" alt="" />
-
-   <p className='bold  text-2xl font-bold'>Primary Care</p>
-  
-  </div>
-  
-  </div>
-
-  {/* //card 2 dentist */}
-
-  <div className="bg-white h-[20rem] w-[20rem] shadow-2xl  border rounded-xl" >
-
-   <div className="text-black flex flex-col items-center mt-16">
- <img  className="  h-[10.5rem]" src="dentist.jpg" alt="" />
-
-   <p className='bold mt-8 text-2xl font-bold'>Dentist</p>
-  
-  </div>
-  
-  </div>
-
-  {/* card 3  OBB-GYN*/}
-
-  <div className="bg-white h-[20rem] w-[20rem] shadow-2xl  border rounded-xl" >
-
-   <div className="text-black flex flex-col items-center mt-16">
- <img  className="  h-[10.5rem]" src="./gyn.jpg" alt="" />
-
-   <p className='bold mt-8 text-2xl font-bold'>OBB-GYN</p>
-  
-  </div>
-  
-  </div>
-
-  {/* card 4 eye */}
-  <div className="bg-white h-[20rem] w-[20rem] shadow-2xl  border rounded-xl" >
-
-   <div className="text-black flex flex-col items-center mt-16">
- <img  className="  h-[10.5rem] rounded-[23rem]" src="./eye.jpg" alt="" />
-
-   <p className='bold mt-8 text-2xl font-bold'>Eye Doctor</p>
-  
-  </div>
-  
-  </div>
-  <div className="bg-white h-[20rem] w-[20rem] shadow-2xl  border rounded-xl" >
-
-   <div className="text-black flex flex-col items-center mt-16">
- <img  className="  h-[10rem]" src="./psyciatrist.jpg" alt="" />
-
-   <p className='bold mt-8 text-2xl font-bold'>Psychiatrist</p>
-  
-  </div>
-  
-  </div>
-  <div className="bg-white h-[20rem] w-[20rem] shadow-2xl  border rounded-xl" >
-
-   <div className="text-black flex flex-col items-center mt-16">
- <img  className="  h-[10rem] rounded-[20rem]" src="cardio.jpg" alt="" />
-
-   <p className='bold mt-8 text-2xl font-bold'>Cardiologist</p>
-  
-  </div>
-  
-  </div>
-  <div className="bg-white h-[20rem] w-[20rem] shadow-2xl  border rounded-xl" >
-
-   <div className="text-black flex flex-col items-center mt-16">
- <img  className="  h-[10.5rem]" src="./psyciatrist copy.jpg" alt="" />
-
-   <p className='bold mt-8 text-2xl font-bold'>Neurologist</p>
-  
-  </div>
-  
-  </div>
-  <div className="bg-white h-[20rem] w-[20rem] shadow-2xl  border rounded-xl" >
-
-   <div className="text-black flex flex-col items-center mt-16">
- <img  className="  h-[10.5rem]" src="./doc2.jpg" alt="" />
-
-   <p className='bold mt-8 text-2xl font-bold'>Dermatologist</p>
-  
-  </div>
-  
-  </div>
-  
-</div>
+      <div className="flex flex-wrap gap-8 mb-32">
+        {CardImage.map((item, idx) => {
+          return (
+            <div key={idx}>
+              <SpecialistCard {...item} />
+            </div>
+          );
+        })}
       </div>
+    </div>
   );
 };
 
