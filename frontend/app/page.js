c62d10531042274bbe2669919fb21bc2ad75ea86
pravalgautam <pravalgautam@Pravals-MacBook-Air.local>
2023-07-30 19:41:28 +0530

    specialities component

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,10 +1,12 @@
 import HeroSection from '@/components/HeroSection'
 import Image from 'next/image'
+import Specialities from '../components/Specialities'
 
 export default function Home() {
   return (
     <div>
      <HeroSection/>
+     <Specialities/>
     </div>
   )
 }
